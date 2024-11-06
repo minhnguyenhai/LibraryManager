5ac8f3568ebc5684380ab241c9f99691a868228f
nguyenminh37 <minh.nv2111011@sis.hust.edu.vn>
2024-11-06 23:05:56 +0700

    update footer

--- fe/src/pages/layout/footer/Footer.js
+++ fe/src/pages/layout/footer/Footer.js
@@ -1,7 +1,22 @@
+import '../footer/Footer.css'
+import image from '../../../assets/img/image.png'
+
 function Footer(){
-    return <h1>
-        Footer
-    </h1>
+    return (
+        <div className='wrapper'>
+            <img src={image}/>
+            <div className='contact'>
+                Địa chỉ: 34, Hà Nội<br/>
+                Mail: group34webhustsupport@gmail.com<br/>
+                Điện thoại: (+84) 345 657 8888
+            </div>
+            <div className='box'>
+                <p className='text-box'>
+                Bản quyền thuộc về 4AM
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
