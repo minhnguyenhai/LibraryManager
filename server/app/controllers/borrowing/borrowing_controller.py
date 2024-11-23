5f25297e20f209b7da0b74ec02e5ebd84c6ccc72
haiminhnguyenn <haiminhc2dh@gmail.com>
2024-11-24 00:04:17 +0700

    Feat: Add API for create Borrow record

--- server/app/controllers/borrowing/borrowing_controller.py
+++ server/app/controllers/borrowing/borrowing_controller.py
@@ -2,7 +2,7 @@ from flask import request, jsonify
 
 from . import borrowing_api
 from ...services.borrowing.borrowing_service import (
-    get_all_borrow_records_of_user
+    get_all_borrow_records_of_user, save_new_borrow_record
 )
 from ...utils.decorators import JWT_required, admin_required
 
@@ -18,6 +18,44 @@ def list_borrow_records_of_user(user_id):
             "user_id": user_id,
             "borrow_records": borrow_records
         }), 200
+    
+    except Exception as e:
+        return jsonify({
+            "error": "Internal server error.",
+            "message": str(e)
+        }), 500
+        
+        
+@borrowing_api.route("/borrowing", methods=["POST"])
+@JWT_required
+@admin_required
+def create_borrow_record():
+    try:
+        data = request.get_json()
+        if data is None:
+            raise ValueError("Invalid JSON data.")
+        
+        REQUIRED_FIELDS = {"user_id", "book_id", "quantity", "borrow_date", "due_date"}
+        missing_fields = {field for field in REQUIRED_FIELDS if data.get(field) is None}
+        
+        if missing_fields:
+            return jsonify({
+                "error": "Missing required fields.",
+                "message": f"Missing fields: {missing_fields}"
+            }), 400
+            
+        new_borrow_record = save_new_borrow_record(data["user_id"], data["book_id"], data["quantity"], data["borrow_date"], data["due_date"])
+        return jsonify({
+            "success": True,
+            "message": "Successfully saved new borrow record.",
+            "borrow_record": new_borrow_record.as_dict()
+        }), 201
+        
+    except ValueError as e:
+        return jsonify({
+            "error": "Invalid data.",
+            "message": str(e)
+        }), 400
     
     except Exception as e:
         return jsonify({
--- server/app/services/borrowing/borrowing_service.py
+++ server/app/services/borrowing/borrowing_service.py
@@ -12,3 +12,17 @@ def get_all_borrow_records_of_user(user_id):
     except Exception as e:
         logging.error(f"Error while fetching all borrow records of a user: {str(e)}")
         raise
+    
+    
+def save_new_borrow_record(user_id, book_id, quantity, borrow_date, due_date):
+    """ Save a new borrow record to the database. """
+    try:
+        new_borrow_record = BorrowRecord(user_id, book_id, quantity, borrow_date, due_date)
+        db.session.add(new_borrow_record)
+        db.session.commit()
+        return new_borrow_record
+    except Exception as e:
+        db.session.rollback()
+        logging.error(f"Error while saving new borrow record: {str(e)}")
+        raise
+    
