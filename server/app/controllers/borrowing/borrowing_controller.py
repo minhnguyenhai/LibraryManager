3433ef6968dcfc15132ca7b6197e64189099b221
haiminhnguyenn <haiminhc2dh@gmail.com>
2024-11-24 00:29:09 +0700

    Feat: Add API for update return date in borrow record

--- server/app/controllers/borrowing/borrowing_controller.py
+++ server/app/controllers/borrowing/borrowing_controller.py
@@ -2,7 +2,8 @@ from flask import request, jsonify
 
 from . import borrowing_api
 from ...services.borrowing.borrowing_service import (
-    get_all_borrow_records_of_user, save_new_borrow_record
+    get_all_borrow_records_of_user, save_new_borrow_record, get_borrow_record_by_id,
+    update_borrow_record_info
 )
 from ...utils.decorators import JWT_required, admin_required
 
@@ -63,3 +64,45 @@ def create_borrow_record():
             "message": str(e)
         }), 500
 
+
+@borrowing_api.route("/borrowing/<borrow_record_id>/return", methods=["PUT"])
+@JWT_required
+@admin_required
+def update_borrow_record(borrow_record_id):
+    try:
+        borrow_record = get_borrow_record_by_id(borrow_record_id)
+        if not borrow_record:
+            return jsonify({
+                "success": False,
+                "message": "Borrow record not found"
+            }), 404
+            
+        data = request.get_json()
+        if data is None:
+            raise ValueError("Invalid JSON data.")
+        
+        return_date = data.get("return_date")
+        if not return_date:
+            return jsonify({
+                "success": False,
+                "message": "Missing field: return_date"
+            }), 400
+        
+        updated_borrow_record = update_borrow_record_info(borrow_record, return_date)
+        return jsonify({
+            "success": True,
+            "message": "Successfully updated return date for borrow record.",
+            "updated_borrow_record": updated_borrow_record.as_dict()
+        }), 200
+        
+    except ValueError as e:
+        return jsonify({
+            "error": "Invalid data.",
+            "message": str(e)
+        }), 400
+    
+    except Exception as e:
+        return jsonify({
+            "error": "Internal server error.",
+            "message": str(e)
+        }), 500
--- server/app/services/borrowing/borrowing_service.py
+++ server/app/services/borrowing/borrowing_service.py
@@ -14,6 +14,18 @@ def get_all_borrow_records_of_user(user_id):
         raise
     
     
+def get_borrow_record_by_id(borrow_record_id):
+    """ Fetch a borrow record by its id. """
+    try:
+        borrow_record = BorrowRecord.query.get(borrow_record_id)
+        if not borrow_record:
+            return None
+        return borrow_record.as_dict()
+    except Exception as e:
+        logging.error(f"Error while fetching borrow record by id: {str(e)}")
+        raise
+    
+    
 def save_new_borrow_record(user_id, book_id, quantity, borrow_date, due_date):
     """ Save a new borrow record to the database. """
     try:
@@ -26,3 +38,14 @@ def save_new_borrow_record(user_id, book_id, quantity, borrow_date, due_date):
         logging.error(f"Error while saving new borrow record: {str(e)}")
         raise
     
+    
+def update_borrow_record_info(borrow_record, return_date):
+    """ Update a borrow record in the database. """
+    try:
+        borrow_record.return_date = return_date
+        db.session.commit()
+        return borrow_record
+    except Exception as e:
+        db.session.rollback()
+        logging.error(f"Error while updating borrow record: {str(e)}")
+        raise
