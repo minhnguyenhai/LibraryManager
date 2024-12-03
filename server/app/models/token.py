5d5312beea478cb6fe6ded206e6952a93898ed2c
haiminhnguyenn <haiminhc2dh@gmail.com>
2024-12-04 01:05:57 +0700

    Fix: Fix bug 'can't compare offset-naive and offset-aware datetimes'

--- server/app/models/token.py
+++ server/app/models/token.py
@@ -18,8 +18,8 @@ class Token(db.Model):
     reset_token: Mapped[Optional[str]] = mapped_column(String, nullable=True)
     verification_code: Mapped[Optional[str]] = mapped_column(String(6), nullable=True)
     reset_code: Mapped[Optional[str]] = mapped_column(String(6), nullable=True)
-    verification_code_expiration: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
-    reset_code_expiration: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
+    verification_code_expiration: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True), nullable=True)
+    reset_code_expiration: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True), nullable=True)
     
     user_id: Mapped[str] = mapped_column(ForeignKey("users.id"), unique=True, nullable=False)
     user: Mapped["User"] = relationship("User", back_populates="token")
