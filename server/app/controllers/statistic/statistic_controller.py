f4e0159498f193d601f9993cd4dfc989b471690a
minh4923 <minh0367913297@gmail.com>
2024-12-07 05:34:28 +0700

    api statistic bool

--- server/app/controllers/statistic/statistic_controller.py
+++ server/app/controllers/statistic/statistic_controller.py
@@ -1,12 +1,23 @@
 from flask import jsonify
 from . import statistic_api
-from ...services.statistic.statistic_service import get_user_statistics  # Import dịch vụ thống kê người dùng
+from ...services.statistic.statistic_service import(
+    get_book_statistic, get_user_statistic
+)
 
 @statistic_api.route('/user-statistics', methods=['GET'])
 def user_statistics():
     """Endpoint to get total user count and new users this month"""
     try:
-        stats = get_user_statistics()
+        stats = get_user_statistic()
+        return jsonify(stats), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@statistic_api.route('/book-statistics', methods=['GET'])
+def book_statistics():
+    """Endpoint to get total book count and new books added this month"""
+    try:
+        stats = get_book_statistic()
         return jsonify(stats), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
--- server/app/services/statistic/statistic_service.py
+++ server/app/services/statistic/statistic_service.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime
 from app import db
 from ...models.user import User
+from ...models.book import Book
 
 def get_user_statistic():
     """Fecth total number of users and new users in the current month"""
@@ -16,4 +17,21 @@ def get_user_statistic():
         }
     except Exception as e:
         logging.error(f"Error while fetching user statistics: {str(e)}")
+        raise
+
+
+def get_book_statistic():
+    """Fetch total number of books and new books added in the current month."""
+    try:
+        today = datetime.today()
+        first_day_of_month = today.replace(day=1)
+        total_books = Book.query.count() 
+        new_books_this_month = Book.query.filter(Book.created_at >= first_day_of_month).count()
+        return {
+            "total_books": total_books,
+            "new_books_this_month": new_books_this_month
+        }
+        
+    except Exception as e:
+        logging.error(f"Error while fetching book statistics: {str(e)}")
         raise
